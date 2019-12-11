191f431d25f40700f2bb45bd5bd3c8294428835e
Matt Hunzinger <matthunz2@gmail.com>
2019-12-11 16:36:29 -0500

    Impl Service<Uri> for Client

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -2,6 +2,9 @@ use crate::{handshake, WebSocket};
 use hyper::client::conn::Builder;
 use hyper::client::service::Connect;
 use hyper::{header, Body, Request, Uri};
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 use tower_service::Service;
 
 mod connect;
@@ -23,33 +26,7 @@ impl Client {
         Self::default()
     }
     pub async fn connect(&mut self, uri: Uri) -> hyper::Result<WebSocket> {
-        let mut svc = self.http.call(uri).await?;
-
-        // TODO don't unwrap
-        let key = handshake::generate().await.unwrap();
-        let req = Request::builder()
-            .header(header::CONNECTION, header::UPGRADE)
-            .header(header::UPGRADE, "websocket")
-            .header("Sec-WebSocket-Key", &key)
-            .body(Body::empty())
-            .unwrap();
-        let res = svc.call(req).await?;
-
-        if let Some(accept) = handshake::get_accept(&res) {
-            // TODO don't unwrap
-            let clone = handshake::accept(&key).await.unwrap();
-
-            if accept == &clone {
-                res.into_body()
-                    .on_upgrade()
-                    .await
-                    .map(WebSocket::from_upgraded)
-            } else {
-                unimplemented!()
-            }
-        } else {
-            unimplemented!()
-        }
+        self.call(uri).await
     }
 }
 
@@ -57,5 +34,49 @@ impl<P> From<Builder> for Client<P> {
     fn from(builder: Builder) -> Self {
         let http = Connect::new(WsConnector::new(), builder);
         Self { http }
+    }
+}
+
+impl Service<Uri> for Client {
+    type Response = WebSocket;
+    type Error = hyper::Error;
+    type Future = Pin<Box<dyn Future<Output = hyper::Result<Self::Response>> + Send + 'static>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context) -> Poll<hyper::Result<()>> {
+        Ok(()).into()
+    }
+
+    fn call(&mut self, uri: Uri) -> Self::Future {
+        let svc_fut = self.http.call(uri);
+
+        Box::pin(async move {
+            let mut svc = svc_fut.await?;
+
+            // TODO don't unwrap
+            let key = handshake::generate().await.unwrap();
+            let req = Request::builder()
+                .header(header::CONNECTION, header::UPGRADE)
+                .header(header::UPGRADE, "websocket")
+                .header("Sec-WebSocket-Key", &key)
+                .body(Body::empty())
+                .unwrap();
+
+            let res = svc.call(req).await?;
+            if let Some(accept) = handshake::get_accept(&res) {
+                // TODO don't unwrap
+                let clone = handshake::accept(&key).await.unwrap();
+
+                if accept == &clone {
+                    res.into_body()
+                        .on_upgrade()
+                        .await
+                        .map(WebSocket::from_upgraded)
+                } else {
+                    unimplemented!()
+                }
+            } else {
+                unimplemented!()
+            }
+        })
     }
 }
