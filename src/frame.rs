965ccecd59cb6d215797ca4d9281464f5d41957c
Matt Hunzinger <matthunz2@gmail.com>
2019-12-11 22:26:07 -0500

    Add Opcode::to_byte and impl From<u8>

--- src/frame.rs
+++ src/frame.rs
@@ -1,13 +1,46 @@
 use crate::Payload;
 
+#[derive(Debug, PartialEq)]
 pub enum Opcode {
+    Continue,
+    Text,
     Binary,
     Close,
-    Continue,
     Ping,
     Pong,
-    Reserved,
-    Text,
+    Reserved(u8),
+}
+
+impl Opcode {
+    pub fn to_byte(&self) -> u8 {
+        use Opcode::*;
+
+        match self {
+            Continue => 1,
+            Text => 2,
+            Binary => 3,
+            Close => 8,
+            Ping => 9,
+            Pong => 10,
+            Reserved(op) => op,
+        }
+    }
+}
+
+impl From<u8> for Opcode {
+    fn from(opcode: u8) -> Opcode {
+        use Opcode::*;
+
+        match opcode {
+            0 => Continue,
+            1 => Text,
+            2 => Binary,
+            8 => Close,
+            9 => Ping,
+            10 => Pong,
+            op => Reserved(op),
+        }
+    }
 }
 
 pub struct Frame<P = Payload> {
